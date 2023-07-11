f6b033d56722a4bb7185b7668672097d784465c0
Veverlyy <128685689+Veverlyy@users.noreply.github.com>
2023-07-11 13:23:32 +0200

    Commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
 
     public float runSpeed = 40f;
-
     float horizontalMove = 0f;
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
     bool jump = false;
 
     // Update is called once per frame
@@ -25,6 +28,46 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("IsJumping", true);
         }
 
+        Attack();
+
+        ChargingKi();
+
+    }
+
+    void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
+        }
+    }
+
+    void ChargingKi()
+    {
+        if (Input.GetButtonDown("ChargingKi"))
+        {
+            animator.SetBool("IsChargingKi", true);
+        }
+
+        if (Input.GetButtonUp("ChargingKi"))
+        {
+            animator.SetBool("IsChargingKi", false);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
     public void OnLanding()
